e6e4dbb031d90b338e5cc41934cc4ba0bb15a49c
GuiDutra21 <221021984@aluno.unb.br>
2026-04-22 07:08:17 -0300

    apenas corrige comentario

--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,4 @@
-#As tupleas sao imutaveis, e possível acrescentar um valor, mas não alterar os valores já existente 
+#As tuplas sao imutaveis, é possível acrescentar um valor, mas não alterar os valores já existente 
 # e também não é possível apagar um elemento apenas a tupla toda
 #As tuplas podem ser formadas por qualquer tipo de dado
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
